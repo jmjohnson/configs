005d83b955c2f0d4099ee7544c1222bf927ba2f3
Jeff Johnson <jeffreyjohnson@google.com>
2013-01-25 13:38:52 -0800

    This didnt seem to work the first time I tried it. Gonna commit this and continue debugging for now.

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,16 @@
 import XMonad
-import XMonad.Config.Gnome
+import XMonad.Config.Gnome (gnomeConfig)
+import XMonad.Util.CustomKeys (customKeys)
 
-main = xmonad gnomeConfig
+main = xmonad $ gnomeConfig
+{-
+  keys = customKeys delKeys insKeys }
+  where
+    delKeys :: XConfig l -> [(KeyMask, KeySym)]
+    delKeys XConfig { modMask = modMask } =
+      [ (modMask, xK_p) ]
+
+    insKeys :: XConfig l -> [((KeyMask, KeySym), X())]
+    insKeys XConfig { modMask = modMask } =
+      [ ((modMask, xK_p), spawn "dmenu_run") ]
+-}
