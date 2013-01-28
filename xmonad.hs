4b6a9f22e97850dd180ed8393b1186ff246a5633
Jeff Johnson <jeffreyjohnson@google.com>
2013-01-28 13:19:11 -0800

    Use the s and d keys to swap between xinerama screens.
    Just a little more within reach.

--- xmonad.hs
+++ xmonad.hs
@@ -1,3 +1,6 @@
+import qualified XMonad.StackSet as W
+import Data.Bits((.|.))
+
 import XMonad
 import XMonad.Config.Gnome (gnomeConfig)
 import XMonad.Util.CustomKeys (customKeys)
@@ -9,8 +12,13 @@ main = xmonad $ gnomeConfig {
   where
     delKeys :: XConfig l -> [(KeyMask, KeySym)]
     delKeys XConfig { modMask = modMask } =
-      [ (modMask, xK_p) ]
+      [ (modMask, xK_p), (modMask, xK_w), (modMask, xK_e) ]
 
     insKeys :: XConfig l -> [((KeyMask, KeySym), X())]
     insKeys XConfig { modMask = modMask } =
       [ ((modMask, xK_p), spawn "dmenu_run") ]
+      ++
+      -- Use asd to switch between active workspaces
+      [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+       | (key, sc) <- zip [xK_s, xK_d] [0..],
+         (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
