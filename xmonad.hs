ee5d164847e05b5ae1ba090a50c218355fb6513a
Jeff Johnson <jeffreyjohnson@google.com>
2013-01-29 09:41:28 -0800

    formatting

--- xmonad.hs
+++ xmonad.hs
@@ -10,15 +10,16 @@ main = xmonad $ gnomeConfig {
   logHook = return ()  -- TODO: Log something!
   }
   where
+    -- Remap mod-p to dmenu and mod-[sd] to swapping xinerama screens.
     delKeys :: XConfig l -> [(KeyMask, KeySym)]
     delKeys XConfig { modMask = modMask } =
       [ (modMask, xK_p), (modMask, xK_w), (modMask, xK_e) ]
 
-    insKeys :: XConfig l -> [((KeyMask, KeySym), X())]
-    insKeys XConfig { modMask = modMask } =
-      [ ((modMask, xK_p), spawn "dmenu_run") ]
-      ++
-      -- Use asd to switch between active workspaces
+    dmenuBindings = [ ((modMask, xK_p), spawn "dmenu_run") ]
+    swapWorkspaceBindings =
       [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
        | (key, sc) <- zip [xK_s, xK_d] [0..],
          (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+    insKeys :: XConfig l -> [((KeyMask, KeySym), X())]
+    insKeys XConfig { modMask = modMask } = dmenuBindings ++ swapWorkspaceBindings
