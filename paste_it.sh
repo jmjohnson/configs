1ac41f71d906019d151bdac0649cbe53547c5ac0
Jeff Johnson <energy.nexus@gmail.com>
2015-11-22 17:03:07 -0500

    Typo

--- paste_it.sh
+++ paste_it.sh
@@ -3,4 +3,4 @@
 SAVED_CLIP="`xclip -selection c -o`"
 printf "$@" | xclip -selection c -in # Reads from stdin
 xdotool key ctrl+shift+v
-printf "$SAVED_CLIP" | xclip -selection c
+printf "$SAVED_CLIP" | xclip -selection c -in
