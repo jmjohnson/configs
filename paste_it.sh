8b6b70579deaa6f16afba07199e0d03e61648d80
Jeff Johnson <energy.nexus@gmail.com>
2015-12-12 18:23:13 -0500

    Update paste_it.sh

--- paste_it.sh
+++ paste_it.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SAVED_CLIP="`xclip -selection c -o`"
-printf "$@" | xclip -selection c -in # Reads from stdin
+printf "$*" | xclip -selection c -in # Reads from stdin
 xdotool key ctrl+shift+v
 printf "$SAVED_CLIP" | xclip -selection c -in
