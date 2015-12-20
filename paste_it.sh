f49b58ad94d3f9325b7c4d30384e813436cb7c5f
Jeff Johnson <energy.nexus@gmail.com>
2015-12-20 14:44:41 -0500

    Paste it now uses ctr+v all the time.

--- paste_it.sh
+++ paste_it.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SAVED_CLIP="`xclip -selection c -o`"
-printf "$@" | xclip -selection c -in # Reads from stdin
-xdotool key shift+Insert
-printf "$SAVED_CLIP" | xclip -selection c -in
+SAVED_CLIP="`xclip -selection clipboard -o`"
+printf "$*" | xclip -selection clipboard -in # Reads from stdin
+xdotool key ctrl+v
+printf "$SAVED_CLIP" | xclip -selection clipboard -in
