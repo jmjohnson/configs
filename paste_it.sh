ee25a50945c938d3c6149691c52ae376e581a961
Jeff Johnson <energy.nexus@gmail.com>
2016-01-19 15:35:11 -0500

    Finally got paste_it synchronizing!

--- paste_it.sh
+++ paste_it.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SAVED_CLIP="`xclip -selection clipboard -o`"
-printf "$*" | xclip -selection clipboard -in # Reads from stdin
+TMP_PIPE=<(printf "$*" | xclip -verbose -loops 0 -selection clipboard -in 2>&1 | tee >(grep -m1 "Waiting for sel") > /dev/null ) # Reads from stdin
+read < TMP_PIPE
 xdotool key ctrl+v
 printf "$SAVED_CLIP" | xclip -selection clipboard -in
