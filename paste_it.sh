4809b1705c1cf5b950ec8a81e9216bb808571330
Jeff Johnson <energy.nexus@gmail.com>
2016-01-22 17:07:59 -0500

    Expand variable.

--- paste_it.sh
+++ paste_it.sh
@@ -2,6 +2,6 @@
 
 SAVED_CLIP="`xclip -selection clipboard -o`"
 TMP_PIPE=<(printf "$*" | xclip -verbose -loops 0 -selection clipboard -in 2>&1 | tee >(grep -m1 "Waiting for sel") > /dev/null ) # Reads from stdin
-read < TMP_PIPE
+read < $TMP_PIPE
 xdotool key ctrl+v
 printf "$SAVED_CLIP" | xclip -selection clipboard -in
