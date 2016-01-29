fac1d988869262a8a3d17261590219f26e3d12bf
Jeff Johnson <energy.nexus@gmail.com>
2016-01-29 14:41:06 -0500

    Finally figured out how to properly wait for this damn thing.

--- paste_it.sh
+++ paste_it.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SAVED_CLIP="`xclip -selection clipboard -o`"
-TMP_PIPE=<(printf "$*" | xclip -verbose -loops 0 -selection clipboard -in 2>&1 | tee >(grep -m1 "Waiting for sel") > /dev/null ) # Reads from stdin
-read < $TMP_PIPE
+printf "$*" | xclip -selection clipboard -in & # Reads from stdin
+wait
+
 xdotool key ctrl+v
 printf "$SAVED_CLIP" | xclip -selection clipboard -in
