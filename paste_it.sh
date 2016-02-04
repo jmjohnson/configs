59b7aa09e723ffa26d220bf948235a90cc6e6fdf
Jeff Johnson <energy.nexus@gmail.com>
2016-02-04 10:58:26 -0500

    Paste it now just sleeps... I give up on race conds...

--- paste_it.sh
+++ paste_it.sh
@@ -3,6 +3,7 @@
 SAVED_CLIP="`xclip -selection clipboard -o`"
 printf "$*" | xclip -selection clipboard -in & # Reads from stdin
 wait
+sleep 0.1
 
 xdotool key ctrl+v
 printf "$SAVED_CLIP" | xclip -selection clipboard -in
