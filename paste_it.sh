2d17f5f50ef2c294c87877d161465319f7000c05
Jeff Johnson <energy.nexus@gmail.com>
2016-02-19 14:14:31 -0500

    No need for this subshell with xsel, I think.

--- paste_it.sh
+++ paste_it.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Save the clipboard to a variable
 SAVED_CLIP="`xclip -selection clipboard -o`"
-( printf "$*" |  xsel -b -i ; ) & # Reads from stdin
-wait
 
+# Send this script's arguments to the clipboard selection.
+## Presumably, xsel attaches the cliboard selection to this currently running program.
+printf "$*" |  xsel -b -i
+xsel --keep &
+wait # May not be needed..?
+
+# Send the paste command.
+## TODO: Use a default ctrl v and add special cases for gnome terminal.
 xdotool key ctrl+v
 
-printf "$SAVED_CLIP" | xsel -b -i && xsel --keep &
-wait
-disown
+# Restore the clipboard contents.
+printf "$SAVED_CLIP" | xsel -b -i
+
+# Ask xsel to fork into a new process group and hold onto the selection... This will only work for text selections...
+xsel --keep
