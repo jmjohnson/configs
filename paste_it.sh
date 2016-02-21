e0828e1b90bf8921343644888332c795502a3f12
Jeff Johnson <energy.nexus@gmail.com>
2016-02-21 13:07:46 -0500

    Yet more accurrate and consistent pasting.
    Especially with chrome

--- paste_it.sh
+++ paste_it.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Save the clipboard to a variable
-SAVED_CLIP="`xclip -selection clipboard -o`"
+SAVED_CLIP="`xsel --clipboard -o`"
 
 # Send this script's arguments to the clipboard selection.
 ## Presumably, xsel attaches the cliboard selection to this currently running program.
+xsel --clipboard --clear
 printf "$*" |  xsel -b -i
-xsel --keep &
-wait # May not be needed..?
 
 # Send the paste command.
 ## TODO: Use a default ctrl v and add special cases for gnome terminal.
@@ -15,6 +14,3 @@ xdotool key ctrl+v
 
 # Restore the clipboard contents.
 printf "$SAVED_CLIP" | xsel -b -i
-
-# Ask xsel to fork into a new process group and hold onto the selection... This will only work for text selections...
-xsel --keep
